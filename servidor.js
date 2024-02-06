2e444a31bb437e0429d0e53ad93d6950c646c431
Francisfox <francismarfox@hotmail.com>
2024-02-06 13:38:25 +0000

    -

--- servidor.js
+++ servidor.js
@@ -8,16 +8,18 @@
 
 import express from 'express';
 import http from 'http'
-import  Socket  from 'socket.io';
+import { Socket } from 'socket.io';
+//import  Socket  from 'socket.io';
 //const express = require('express');
 //const http = require('http');
 
 const aplicacao = express();
 
 const servidorHttp = http.createServer(aplicacao);
-const io = require('sockt.io')(servidorHttp);
+const io = (Socket);
 
 aplicacao.use(express.static('public'));
+
 servidorHttp.listen(3000 , ()=>{
     console.log(`> Servidor linstening om porte:3000`)
 })
